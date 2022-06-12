fe4be10e8d5e9c98f28eac6ddb2a534dd039f684
Burak Ozdemir <burakozdemir791@gmail.com>
2022-06-12 00:04:43 -0700

    Integrating redux

---
+++ src/features/counter/userSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+// import { fetchCount } from "./counterAPI";
+
+// export const incrementAsync = createAsyncThunk(
+//   "counter/fetchCount",
+//   async (amount) => {
+//     const response = await fetchCount(amount);
+//     return response.data;
+//   }
+// );
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: null,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },  
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+//Selectors
+export const selectUser = (state) => state.user.user;
+
+export default userSlice.reducer;
