443c5d394e288ca58d250201097aae9db03bd860
Burak Ozdemir <burakozdemir791@gmail.com>
2022-07-04 11:57:40 -0700

    Update firebase credential

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,21 @@
 import firebase from "firebase";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBpqCCP3Dy160aXkMbNFLJBNLf_IJv3zo0",
+//   authDomain: "linkedin-clone-16793.firebaseapp.com",
+//   projectId: "linkedin-clone-16793",
+//   storageBucket: "linkedin-clone-16793.appspot.com",
+//   messagingSenderId: "311771204088",
+//   appId: "1:311771204088:web:525bc12c800281d2962e0d",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBpqCCP3Dy160aXkMbNFLJBNLf_IJv3zo0",
-  authDomain: "linkedin-clone-16793.firebaseapp.com",
-  projectId: "linkedin-clone-16793",
-  storageBucket: "linkedin-clone-16793.appspot.com",
-  messagingSenderId: "311771204088",
-  appId: "1:311771204088:web:525bc12c800281d2962e0d",
+  apiKey: "AIzaSyBsZIU8218ObLohP93Ii5PLamFUI1Iat3I",
+  authDomain: "linkedin-clone-129a6.firebaseapp.com",
+  projectId: "linkedin-clone-129a6",
+  storageBucket: "linkedin-clone-129a6.appspot.com",
+  messagingSenderId: "957593861169",
+  appId: "1:957593861169:web:aee76c901a510f14252910"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
